961deee3c482f8af3b256f3d92c7aa45c185a2ee
Shahed1998 <shahedchowdhury120@gmail.com>
2022-02-28 21:26:26 +0600

    login route created

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Login;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Login routes
+Route::get('/login', [Login::class, 'loginGet'])->name('login');
