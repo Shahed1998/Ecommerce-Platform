2ec876a9fa01e7c5deeabc1f5561755b12986cc4
Shahed1998 <shahedchowdhury120@gmail.com>
2022-03-01 19:52:23 +0600

    customer registration validation done

--- app/Http/Controllers/customer/registrationController.php
+++ app/Http/Controllers/customer/registrationController.php
@@ -7,8 +7,48 @@ use Illuminate\Http\Request;
 
 class registrationController extends Controller
 {
+    // Validator function
+    private function form_validator($req){
+        $validated_data = $this->validate($req, [
+            "uname"=>"required|min:5",
+            "email"=>"required|regex:/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix",
+            "password"=>"required|min:8",
+            "c_password"=>"required|min:8",
+            "gender"=>"required",
+            "contact"=>"required|min:2|max:15",
+            "present_address"=>"required",
+            "permanent_address"=>"required",
+            "customer_image"=>"required|mimes:jpg,png,jpeg"
+        ],[
+            "uname.required"=>"User name is required",
+            "uname.min"=>"Username must be at least 5 characters long",
+            "email.required"=>"Email is required",
+            "password.required"=>"Password is required",
+            "password.min"=>"Password must be more than 8 characters long",
+            "c_password.required"=>"Confirm password is required",
+            "c_password.min"=>"Confirm password must be more than 8 characters long",
+            "gender.required"=>"Gender must be selected",
+            "contact.required"=>"Contact number is required",
+            "contact.min"=>"Contact number must be of minimum 2 characters",
+            "contact.max"=>"Contact number must be of maximum 15 characters",
+            "present_address.required"=>"Present address is required",
+            "permanent_address.required"=>"Permanent address is required",
+            "customer_image.required"=>"Image is required",
+            "customer_image.mimes"=>"Incorrect image format"
+        ]);
+
+        return $validated_data;
+    }
+
     //get registration page
     public function getRegister(){
         return view('register')->with('pageName', 'Sign up');
+    }
+
+    // post registration page
+    public function postRegister(Request $req){
+
+        $this->form_validator($req);
+        
     }
 }
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,50 +1,132 @@
 @extends('layouts.home')
 @section('content')
-    <form action="" method="post">
+    <form action="" method="post" enctype="multipart/form-data">
         {{@csrf_field()}}
         <table>
             <tr>
                 <td>Username:</td>
-                <td><input type="text" name="uname" id=""></td>
+                <td><input type="text" name="uname" id="" value="{{old('uname')}}"></td>
+                <td>
+                    <span class="err" style="color:red;">
+                        @error("uname")
+                            {{$message}}
+                        @enderror
+                    </span>
+                </td>
             </tr>
             <tr>
                 <td>Email:</td>
-                <td><input type="text" name="email" id=""></td>
+                <td><input type="text" name="email" id="" value="{{old('email')}}"></td>
+                <td>
+                    <span class="err" style="color:red;">
+                        @error("email")
+                            {{$message}}
+                        @enderror
+                    </span>
+                </td>
             </tr>
             <tr>
                 <td>Password:</td>
-                <td><input type="password" name="password" id=""></td>
+                <td><input type="password" name="password" id="" value="{{old('password')}}"></td>
+                <td>
+                    <span class="err" style="color:red;">
+                        @error("password")
+                            {{$message}}
+                        @enderror
+                    </span>
+                </td>
             </tr>
             <tr>
                 <td>Confirm password:</td>
-                <td><input type="password" name="c_password" id=""></td>
+                <td><input type="password" name="c_password" id="" value="{{old('c_password')}}"></td>
+                <td>
+                    <span class="err" style="color:red;">
+                        @error("c_password")
+                            {{$message}}
+                        @enderror
+                    </span>
+                </td>
             </tr>
             <tr>
                 <td>Gender:</td>
                 <td>
-                    <select name="gender" id="" >
-                        <option value="" disabled selected>Select gender below</option>
-                        <option value="Male">Male</option>
-                        <option value="Female">Female</option>
-                        <option value="Others">Others</option>
+                    <select name="gender" id="">
+
+                        <option value="" disabled selected>Select the option below</option>
+
+                        @if(old('gender') == "Male") <option value="Male" selected>Male</option>
+                        @else <option value="Male">Male</option>
+                        @endif
+
+                        @if(old('gender') == "Female") <option value="Female" selected>Female</option>
+                        @else <option value="Female">Female</option>
+                        @endif
+                        
+                        @if(old('gender') == "Others") <option value="Others" selected>Others</option>
+                        @else <option value="Others">Others</option>
+                        @endif
+
                     </select>
+                </td>
+                <td>
+                    <span class="err" style="color:red;">
+                        @error("gender")
+                            {{$message}}
+                        @enderror
+                    </span>
                 </td>
             </tr>
             <tr>
                 <td>Contact number:</td>
-                <td><input type="text" name="contact" id=""></td>
+                <td>
+                    <select name="country_code" id="">
+                        <option value="+880" selected>+880</option>
+                        <option value="+91">+91</option>
+                        <option value="+92">+92</option>
+                        <option value="+86">+86</option>
+                    </select>
+                    <input type="text" name="contact" id="" value="{{old('contact')}}">
+                </td>
+                <td>
+                    <span class="err" style="color:red;">
+                        @error("contact")
+                            {{$message}}
+                        @enderror
+                    </span>
+                </td>
             </tr>
             <tr>
                 <td>Present address:</td>
-                <td><textarea name="present_address" id="" cols="25" rows="5"></textarea></td>
+                <td><textarea name="present_address" id="" cols="25" rows="5">{{old('present_address')}}</textarea></td>
+                <td>
+                    <span class="err" style="color:red;">
+                        @error("present_address")
+                            {{$message}}
+                        @enderror
+                    </span>
+                </td>
             </tr>
             <tr>
                 <td>Permanent address:</td>
-                <td><textarea name="permanent_address" id="" cols="25" rows="5"></textarea></td>
+                <td><textarea name="permanent_address" id="" cols="25" rows="5">{{old('permanent_address')}}</textarea></td>
+                <td>
+                    <span class="err" style="color:red;">
+                        @error("permanent_address")
+                            {{$message}}
+                        @enderror
+                    </span>
+                </td>
             </tr>
             <tr>
                 <td>Upload image:</td>
                 <td><input type="file" name="customer_image" id=""></td>
+                <td>
+                    <span class="err" style="color:red;">
+                        @error("customer_image")
+                            {{$message}}
+                        @enderror
+                    </span>
+                </td>
             </tr>
             <tr>
                 <td></td>
